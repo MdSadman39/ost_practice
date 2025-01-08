9f39bed41a98608d4b8bb26f5003354390bada68
MdSadman39 <shaon.cse.pust@gmail.com>
2025-01-08 23:49:12 +0600

    asdf

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,24 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-// NamedNavigation,StatefullWidget
+
+/// MediaQuery | Wrap | LayoutBuilder | OrientationBuilder
 
 void main() {
-  runApp(SampleApp());
+  runApp(const SimpleApp());
 }
 
-class SampleApp extends StatelessWidget {
-  const SampleApp({super.key});
+class SimpleApp extends StatelessWidget {
+  const SimpleApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      // initialRoute: '/home',
+      // initialRoute: '/',
       // routes: {
-      //   '/home': (context) => Home(),
+      //   '/': (context) => Home(),
       //   '/profile': (context) => Profile(),
       //   '/settings': (context) => Settings(),
       // },
-      home: CounterScreen(),
+      home: Home(),
     );
   }
 }
@@ -29,147 +28,66 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    // Size screenSize = MediaQuery.sizeOf(context);
+    print(screenSize.width);
+    print(screenSize.height);
+    print(screenSize.flipped);
+    print(screenSize.aspectRatio);
+    print(screenSize.longestSide);
+    print(screenSize.shortestSide);
+
+    print(MediaQuery.of(context).devicePixelRatio);
+    print(MediaQuery.of(context).orientation);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
-        centerTitle: true,
-        backgroundColor: Colors.green,
       ),
-      body: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              // Navigator.push(
-              //   context,
-              //   MaterialPageRoute(
-              //     builder: (context) => Profile(),
-              //   ),
-              // );
-              Navigator.pushNamed(context, '/profile');
-            },
-            child: Text('Go to Profile'),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class Profile extends StatelessWidget {
-  const Profile({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Profile'),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-      ),
-      body: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              // Navigator.pushReplacement(
-              //   context,
-              //   MaterialPageRoute(
-              //     builder: (context) => Settings(),
-              //   ),
-              // );
-              Navigator.pushReplacementNamed(context, '/settings');
-            },
-            child: Text('Go to Settings'),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class Settings extends StatelessWidget {
-  const Settings({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Settings'),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-      ),
-      body: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              // Navigator.pushAndRemoveUntil(
-              //     context,
-              //     MaterialPageRoute(
-              //       builder: (context) => Home(),
-              //     ),
-              //     (predicate) => false);
-              Navigator.pushNamedAndRemoveUntil(
-                  context, '/home', (predicate) => false);
-            },
-            child: Text('Go to Home'),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-// class CounterScreen extends StatelessWidget {
-//    CounterScreen({super.key});
-//   int Counter=0;
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Counter'),
-//         centerTitle: true,
-//         backgroundColor: Colors.green,
-//       ),
-//       body: Center(
-//         child: Text('Counter value is $Counter'),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: () {
-//           Counter=Counter+1;
-//         },
-//         child: Icon(Icons.add),
-//       ),
-//     );
-//   }
-// }
-
-class CounterScreen extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return CounterScreenState();
-  }
-}
-
-class CounterScreenState extends State<CounterScreen> {
-  int Counter = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Counter'),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-      ),
-      body: Center(
-        child: Text('Counter value is $Counter'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Counter = Counter + 1;
-          setState(() {});
+      // body: Column(
+      //   mainAxisAlignment: MainAxisAlignment.center,
+      //   children: [
+      //     Wrap(
+      //       alignment: WrapAlignment.center,
+      //       crossAxisAlignment: WrapCrossAlignment.start,
+      //       spacing: 8,
+      //       runSpacing: 8,
+      //       children: [
+      //         ElevatedButton(onPressed: () {}, child: Text('Button')),
+      //         ElevatedButton(onPressed: () {}, child: Text('Button')),
+      //         ElevatedButton(onPressed: () {}, child: Text('Button')),
+      //         ElevatedButton(onPressed: () {}, child: Text('Button')),
+      //         ElevatedButton(onPressed: () {}, child: Text('Button')),
+      //         ElevatedButton(onPressed: () {}, child: Text('Button')),
+      //         ElevatedButton(onPressed: () {}, child: Text('Button')),
+      //         ElevatedButton(onPressed: () {}, child: Text('Button')),
+      //         ElevatedButton(onPressed: () {}, child: Text('Button')),
+      //         ElevatedButton(onPressed: () {}, child: Text('Button')),
+      //         ElevatedButton(onPressed: () {}, child: Text('Button')),
+      //         ElevatedButton(onPressed: () {}, child: Text('Button')),
+      //         ElevatedButton(onPressed: () {}, child: Text('Button')),
+      //       ],
+      //     )
+      //   ],
+      // ),
+      // body: Padding(
+      //   padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      //   child: LayoutBuilder(
+      //     builder: (BuildContext context, BoxConstraints constraints) {
+      //       return Center(
+      //         child: Text('${constraints.maxHeight} ${constraints.maxWidth}'),
+      //       );
+      //     }
+      //   ),
+      // ),
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          if (orientation == Orientation.portrait) {
+            return Center(child: Text('Hello World'));
+          } else {
+            return Center(child: Text('Hello another world'));
+          }
         },
-        child:const  Icon(Icons.add),
       ),
     );
   }
