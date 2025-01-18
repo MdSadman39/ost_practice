413c4cd5a6f5d6a7dc2043d407217c025c541137
MdSadman39 <shaon.cse.pust@gmail.com>
2025-01-19 01:29:13 +0600

    asdfsa

---
+++ lib/Screens/Update_todo_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class AddNewTodoScreen extends StatefulWidget {
+  const AddNewTodoScreen({super.key});
+
+  @override
+  State<AddNewTodoScreen> createState() => _AddNewTodoScreenState();
+}
+
+class _AddNewTodoScreenState extends State<AddNewTodoScreen> {
+  final TextEditingController _tittleTEController = TextEditingController();
+  final TextEditingController _descriptionTEController =
+  TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Update todo list'),
+        centerTitle: true,
+        backgroundColor: Colors.amberAccent,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _tittleTEController,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (String? value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Enter Your tittle';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'tittle',
+                    hintText: 'Enter your tittle',
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: _descriptionTEController,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (String? value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Enter Your tittle';
+                    }
+                    return null;
+                  },
+                  maxLines: 3,
+                  decoration: InputDecoration(
+                    labelText: 'description',
+                    hintText: 'Enter your description',
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {}
+                  },
+                  child: const Text('add'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
